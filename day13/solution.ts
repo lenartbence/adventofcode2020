d335e36bae2bc96f4ffb5ea790cac2ab9fae04f3
lenartbence <lenartbence@gmail.com>
2020-12-14 01:23:12 +0100

    Day 13 part 2

--- day13/solution.ts
+++ day13/solution.ts
@@ -23,5 +23,61 @@ const indexOfSmallest = (array: Array<number>) => {
 
 const indexOfMyBus = indexOfSmallest(waitTimes);
 
-// the ID of the earliest bus you can take to the airport multiplied by the number of minutes you'll need to wait for that bus:
 console.log("Part 1: " + buses[indexOfMyBus] * waitTimes[indexOfMyBus]);
+
+// Part 2
+
+const greatestCommonDivisor = (a: number, b: number) => {
+    while (b != 0) {
+        const t = b;
+        b = a % b;
+        a = t;
+    }
+
+    return a
+}
+
+const lowestCommonMultiple = (a: number, b: number) => {
+    if (a > b)
+        return (a / greatestCommonDivisor(a, b)) * b;
+    else
+        return (b / greatestCommonDivisor(a, b)) * a;
+}
+
+const lowestCommonMultipleArray = (array: Array<number>) => {
+    return array.reduce(lowestCommonMultiple, 1);
+}
+
+const getDepartOffsets = (scheduleEntries: Array<string>) => {
+    const result = new Map<number, number>();
+    for (const [i, entry] of scheduleEntries.entries()) {
+        if (entry != "x") {
+            result.set(parseInt(entry), i);
+        }
+    }
+    return result;
+}
+
+const departOffsets = getDepartOffsets(input[1].split(","));
+
+const doBusesAlign = (timestamp: number, busesToCheck: Array<number>) => {
+    for (const b of busesToCheck) {
+        if ((timestamp + departOffsets.get(b)) % b != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+let jump = 1;
+let timestamp = buses[0];
+
+for (let numberOfBusesToCheck = 2; numberOfBusesToCheck <= buses.length; numberOfBusesToCheck++) {
+    while (!doBusesAlign(timestamp, buses.slice(0, numberOfBusesToCheck))) {
+        timestamp += jump;
+    }
+
+    jump = lowestCommonMultipleArray(buses.slice(0, numberOfBusesToCheck));
+}
+
+console.log("Part 2: " + timestamp);
