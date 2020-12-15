331212a6fe9e16e5e9e8e615fa82523ca8cdcb7c
lenartbence <lenartbence@gmail.com>
2020-12-15 23:48:30 +0100

    Day 15 part 2

--- day15/solution.ts
+++ day15/solution.ts
@@ -1,23 +1,29 @@
 const startingNumbers = "8,11,0,19,1,2".split(",").map(x => parseInt(x));
 
-const numbersSpoken = new Map<number, number>();
-
 let lastNumberSpoken: [number, boolean];
-for (let i = 0; i < startingNumbers.length; i++) {
-    numbersSpoken.set(startingNumbers[i], i);
-    lastNumberSpoken = [startingNumbers[i], false];
-}
+const bruteForce = (numbers: Array<number>, target:number) => {
+    const numbersSpoken = new Map<number, number>();
 
-for (let i = startingNumbers.length; i < 2020; i++) {
-    let newNumber = 0;
-    if (lastNumberSpoken[1]) {
-        newNumber = i - 1 - numbersSpoken.get(lastNumberSpoken[0]);
+    for (let i = 0; i < startingNumbers.length; i++) {
+        numbersSpoken.set(startingNumbers[i], i);
+        lastNumberSpoken = [startingNumbers[i], false];
     }
 
-    numbersSpoken.set(lastNumberSpoken[0], i-1);
+    for (let i = startingNumbers.length; i < target; i++) {
+        let newNumber = 0;
+        if (lastNumberSpoken[1]) {
+            newNumber = i - 1 - numbersSpoken.get(lastNumberSpoken[0]);
+        }
 
-    const wasSpokenBefore = numbersSpoken.has(newNumber);
-    lastNumberSpoken = [newNumber, wasSpokenBefore];
+        numbersSpoken.set(lastNumberSpoken[0], i - 1);
 
-    console.log((i + 1) + ". " + newNumber);
+        const wasSpokenBefore = numbersSpoken.has(newNumber);
+        lastNumberSpoken = [newNumber, wasSpokenBefore];
+
+        //console.log((i + 1) + ". " + newNumber);
+    }
 }
+
+bruteForce(startingNumbers, 30000000);
+
+console.log(lastNumberSpoken[0]);
